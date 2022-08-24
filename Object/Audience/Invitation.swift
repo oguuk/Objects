bfa043a1b804b9b635d885f0a95a82469a1d8df3
oguuk <ogw135@gmail.com>
2022-08-24 18:11:44 +0900

    Invitation 구현

---
+++ Object/Audience/Invitation.swift
@@ -0,0 +1,17 @@
+//
+//  Invitation.swift
+//  Object
+//
+//  Created by 오국원 on 2022/08/24.
+//
+
+import Foundation
+
+class Invitation {
+    
+    private var date: Date?
+    
+    init(date: Date) {
+        self.date = date
+    }
+}
---
+++ Object/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Object
+//
+//  Created by 오국원 on 2022/08/24.
+//
+
+import Foundation
+
+print("Hello, World!")
+
